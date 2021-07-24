2afb36ba921c51033be6468424c7aa318adf251d
Azizul Tareq <azizul.tareq@northsouth.edu>
2021-07-24 16:44:23 +0600

    map over all places from database

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react'
-import ReactMapGL, {Marker} from 'react-map-gl';
+import React, { useState, useEffect } from 'react'
+import ReactMapGL, {Marker, Popup} from 'react-map-gl';
 import { ImLocation2 } from 'react-icons/im'
+import { AiFillStar } from 'react-icons/ai'
+import axios from 'axios'
+import { format } from 'timeago.js'
+import './App.css'
 
 function App() {
+  const [pins, setPins] = useState([])
   const [viewport, setViewport] = useState({
     width: "100vw",
     height: "100vh",
@@ -10,6 +15,18 @@ function App() {
     longitude: 90.4208,
     zoom: 4
   });
+
+  useEffect(() => {
+    const getPins = async () => {
+      try {
+        const allPins = await axios.get("/pins");
+        setPins(allPins.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getPins();
+  }, []);
   return (
     <div className="App">
       <ReactMapGL
@@ -18,9 +35,41 @@ function App() {
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapStyle="mapbox://styles/azizultareq/ckr8uqwzr1prq18p0wrxzpndw"
 >
-      <Marker latitude={23.8070} longitude={90.3636} offsetLeft={-20} offsetTop={-10}>
+      
+      {pins.map((p) => (
+        <>
+        <Marker latitude={p.lat} longitude={p.long} offsetLeft={-20} offsetTop={-10}>
         <ImLocation2 style={{ fontSize: viewport.zoom * 7, color: '#FFAA00'}} />
       </Marker>
+
+       <Popup
+          latitude={p.lat}
+          longitude={p.long}
+          closeButton={true}
+          closeOnClick={false}
+          anchor="left" >
+          <div className="card">
+            <label>Place</label>
+            <h4 className="place">{p.title}</h4>
+            <label>Review</label>
+            <p className="desc">{p.desc}</p>
+            <label>Rating</label>
+            <div className="stars">
+              <AiFillStar className="star" />
+              <AiFillStar className="star" />
+              <AiFillStar className="star" />
+              <AiFillStar className="star" />
+              <AiFillStar className="star" />
+            </div>
+            <label>Information</label>
+            <span className="user">Created by <b>{p.username}</b></span>
+            <span className="date">{format(p.createdAt)}</span>
+          </div>
+      </Popup>
+      </>
+
+      ))}
+     
     </ReactMapGL>
     </div>
   );
