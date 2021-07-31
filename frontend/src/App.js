3aca9edd173e3816cb0655103d5eef8247d9c666
Azizul Tareq <azizul.tareq@northsouth.edu>
2021-07-31 12:37:40 +0600

    form added for review a new place & rating fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,9 +7,15 @@ import { format } from 'timeago.js'
 import './App.css'
 
 function App() {
+  const currentuser = "Tareq"
   const [pins, setPins] = useState([])
+  const [newPlace, setNewPlace] = useState(null)
   const [currentPlaceId, setCurrentPlaceId] = useState(null)
+  const [title, setTitle] = useState(null)
+  const [desc, setDesc] = useState(null)
+  const [rating, setRating] = useState(0)
   const [viewport, setViewport] = useState({
+  
     width: "100vw",
     height: "100vh",
     latitude: 23.7612,
@@ -29,9 +35,45 @@ function App() {
     getPins();
   }, []);
 
-  const onMarkerClick = (id) => {
+  const onMarkerClick = (id, lat, long) => {
       setCurrentPlaceId(id)
+      setViewport({
+        ...viewport,
+        latitude: lat,
+        longitude: long 
+      })
+
   }
+
+  const handleAddClick = (e) => {
+    const [long, lat] = e.lngLat
+
+    setNewPlace({
+      long,
+      lat
+    })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPin = {
+      username: currentuser,
+      title,
+      desc,
+      rating,
+      lat: newPlace.lat,
+      long: newPlace.long
+    }
+
+    try {
+      const res = await axios.post('/pins', newPin);
+      setPins([...pins, res.data]);
+      setNewPlace(null)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
   return (
     <div className="App">
       <ReactMapGL
@@ -39,13 +81,14 @@ function App() {
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapStyle="mapbox://styles/azizultareq/ckr8uqwzr1prq18p0wrxzpndw"
+      onDblClick = {handleAddClick}
 >
       
       {pins.map((p) => (
         <>
         <Marker latitude={p.lat} longitude={p.long} offsetLeft={-20} offsetTop={-10}>
-        <ImLocation2 style={{ fontSize: viewport.zoom * 7, color: '#FFAA00', cursor: 'pointer'}}
-        onClick={() => onMarkerClick(p._id)}
+        <ImLocation2 style={{ fontSize: viewport.zoom * 7, color: p.username === currentuser? 'red' : '#FFAA00', cursor: 'pointer'}}
+        onClick={() => onMarkerClick(p._id, p.lat, p.long)}
         />
       </Marker>
 
@@ -64,23 +107,53 @@ function App() {
             <p className="desc">{p.desc}</p>
             <label>Rating</label>
             <div className="stars">
-              <AiFillStar className="star" />
-              <AiFillStar className="star" />
-              <AiFillStar className="star" />
-              <AiFillStar className="star" />
-              <AiFillStar className="star" />
+              {Array(p.rating).fill(<AiFillStar style={{color: 'gold'}} />)}
             </div>
             <label>Information</label>
             <span className="user">Created by <b>{p.username}</b></span>
             <span className="date">{format(p.createdAt)}</span>
           </div>
       </Popup>
-
-        )}
-       
+        )}  
       </>
-
       ))}
+        {newPlace && (
+        <Popup
+          latitude={newPlace.lat}
+          longitude={newPlace.long}
+          closeButton={true}
+          closeOnClick={false}
+          onClose={() => setNewPlace(null)}
+          anchor="left" >
+            <div>
+              <form onSubmit={handleSubmit}>
+                <label>Title</label>
+                <input 
+                  placeholder='Enter a title'
+                  onChange={(e)=>setTitle(e.target.value)}
+                />
+              
+                <label>Review</label>
+                <textarea 
+                  placeholder='Write about this place'
+                  onChange={(e)=>setDesc(e.target.value)}
+                />
+
+             
+                <label>Rating</label>
+                <select onChange={(e)=>setRating(e.target.value)}>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                </select>
+                <button className="submitButton" type="submit">Add Review</button>
+              </form>
+            </div>
+          </Popup>
+
+      )}
      
     </ReactMapGL>
     </div>
