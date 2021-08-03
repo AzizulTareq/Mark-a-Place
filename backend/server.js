913144e9be575c4bb90bf07568d105005e694916
Azizul Tareq <azizul.tareq@northsouth.edu>
2021-08-03 16:11:24 +0600

    frontend and backend running together by using concurrently

--- backend/server.js
+++ backend/server.js
@@ -24,5 +24,24 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use("/api/users", userRoute);
 app.use("/api/pins", pinRoute);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
-app.listen(5000, console.log('App is running..'))
+
+  const PORT = process.env.PORT || 5000
+
+  app.listen(
+    PORT,
+    console.log(
+      `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  )
